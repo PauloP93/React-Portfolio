ba7f97c76ee3d036a34d68ac6d8828e62741f606
Paulo Pimenta <pauloasp93@gmail.com>
2024-12-23 23:36:05 +0000

    Updates

--- src/components/CyberpunkTechlines/CyberpunkTechLines.jsx
+++
@@ -1,6 +0,0 @@
-import "./CyberpunkTechLines.css";
-
-export default function CyberpunkTechLines({ cssClasses = "" }) {
-  let cssClassesTechLines = "tech-lines" + " " + cssClasses;
-  return <img src="./tech_lines.png" className={cssClassesTechLines} />;
-}
--- src/components/Links/CarrouselImg.jsx
+++
@@ -1,9 +0,0 @@
-export default function CarrouselImg({ image }) {
-    return (
-        <img
-            className="w-32 h-32 items-center"
-            src={image.Image}
-            alt={image.Name}
-        ></img>
-    );
-}
